5cce453a6ae27ebad961d0537e81cb30a2f69f48
ones44 <udeveloper@ymail.com>
2013-04-16 21:21:30 +0200

    add index

---
+++ index.php
@@ -0,0 +1,6 @@
+
+<!-- saved from url=(0112)https://raw.github.com/RooCMS/Last-stable-version/4fd16014161ab354374d3e7b93874c78054e66fa/img/icon/16/index.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style type="text/css"></style></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">&lt;?php
+header($_SERVER['SERVER_PROTOCOL'].' 301 Moved Permanently');
+header("Location: /");
+?&gt;</pre></body></html>
